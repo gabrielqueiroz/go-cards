51dcb6c264553c1c611781ab7281b34cd659e5b7
Gabriel Queiroz <gqueiroz@tractionondemand.com>
2018-11-22 17:19:52 -0800

    Add more features to deck - Add write to file - Add read from file - Add shuffle function

---
+++ README.md
@@ -0,0 +1,7 @@
+# Go Cards
+
+Go Cards from Udemy course for Go fundamentals. 
+
+## What's happening here?
+
+It creates a deck of cards and allows you to shuffle and deal cards from the deck.
--- deck.go
+++ deck.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
 )
 
 type deck []string
@@ -21,12 +26,41 @@ func newDeck() deck {
 	return cards
 }
 
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
 }
 
-func deal(d deck, handSize int) (deck, deck) {
-	return d[:handSize], d[handSize:]
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(fielname string) deck {
+	bs, err := ioutil.ReadFile(fielname)
+	if err != nil {
+		fmt.Println("Error", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
